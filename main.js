f2da6e6285d0a87ac7299e08a2a4e5c932a51915
anupamagarg <dolly.singla@gmail.com>
2020-04-26 23:13:43 -0500

    function is returning reverse integer

--- main.js
+++ main.js
@@ -96,3 +96,13 @@ function fizzBuzz() {
 };
 const FizzBuzz= fizzBuzz();
 console.log(FizzBuzz);
+
+// CHALLENGE 4: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {
+  const revString = int.toString().split('').reverse().join('');
+
+  return parseInt(revString) * Math.sign(int);
+}
