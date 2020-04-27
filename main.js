b910296df7b961135f214a315523431b8d6eae55
anupamagarg <dolly.singla@gmail.com>
2020-04-26 23:59:09 -0500

    function is returning next character

--- main.js
+++ main.js
@@ -175,8 +175,18 @@ console.log(MaxCharacter)
 
 //CHALLENGE 6: LETTER CHANGES
 // b should return c and z should return a. It should return the next letter.
-function letterChanges() {
-    console.log("change letters");
+function letterChanges(str) {
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, char => {
+        if (char === 'z' || char === 'Z') {
+          return 'a';
+        } else {
+          return String.fromCharCode(char.charCodeAt() + 1);
+        }
+      });
+    
+      newStr = newStr.replace(/a|e|i|o|u/gi, vowel => vowel.toUpperCase());
+    
+      return newStr;
 }
-
-console.log(letterChanges());
+const LetterChanges = letterChanges("Welcome")
+console.log(LetterChanges);// returns xfmdpnf
