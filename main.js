80561312cb31237af58dffb8b5836af69675fcc7
anupamagarg <dolly.singla@gmail.com>
2020-04-29 22:51:26 -0500

    funtion returns sum of prime numbers

--- main.js
+++ main.js
@@ -272,3 +272,26 @@ console.log(AddAll);
 //CHALLENGE 11: SUM ALL PRIMES
 // Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
+
+function sumAllPrimes(num) {
+  let total = 0;
+
+  function checkForPrime(i) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 2; i <= num; i++) {
+    if (checkForPrime(i)) {
+      total += i;
+    }
+  }
+  return total;
+}
+
+const SumAllPrimes = sumAllPrimes(20);
+console.log(SumAllPrimes);
