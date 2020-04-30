ee896929d3682cd0e87fa3d29c8960634082060d
anupamagarg <dolly.singla@gmail.com>
2020-04-29 22:35:58 -0500

    fuction to return sum

--- main.js
+++ main.js
@@ -256,3 +256,12 @@ console.log(IsAnagramx);
 // CHALLENGE 10: ADD ALL NUMBERS
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 // ex. addAll(2,5,6,7) === 20
+
+function addAll() {
+  var args = Array.prototype.slice.call(arguments);
+  var total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+}
