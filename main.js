7eefc2fb236793b89fae9ccfed0fc2ea20ecd1bc
anupamagarg <dolly.singla@gmail.com>
2020-04-29 22:42:26 -0500

    find prime number

--- main.js
+++ main.js
@@ -267,3 +267,8 @@ function addAll() {
 }
 const AddAll = addAll(4,2,5,6);
 console.log(AddAll);
+
+
+//CHALLENGE 11: SUM ALL PRIMES
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// ex. sumAllPrimes(10) == 17
