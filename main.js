dbb6dac3ffc64d437a33bd170c2a9f07c9b3265e
anupamagarg <dolly.singla@gmail.com>
2020-05-24 23:24:29 -0500

    function eliminats the duplicate from array

--- main.js
+++ main.js
@@ -440,3 +440,24 @@ console.log(factorial(4));
 
 
 //Challenge 18 Eliminate duplicate no from array by using hashtable
+
+// eliminate duplicate no from array
+//always use hash table
+
+const duplicate = arr => {
+  var hashTable = {};
+  for (let i = 0; i < arr.length; i++) {
+      if(hashTable[arr[i]] === 1)
+      {
+          arr.splice(i,1);
+          --i;
+      }
+      else
+          hashTable[arr[i]] = 1;
+  }
+  return arr;
+}
+
+const array = [3,5,3,2,34,6,2,6,3,56,4,87];
+console.log(array);
+console.log(duplicate(array));
