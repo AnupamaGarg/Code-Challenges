05b59e365d66fe3e65c5a80588d6995258047fa4
anupamagarg <dolly.singla@gmail.com>
2020-05-29 22:07:59 -0500

    replace space with character

--- main.js
+++ main.js
@@ -494,3 +494,18 @@ console.log(convert(28.12));
 // Challenge 20 Replace each space with %20
 //in a given string
 
+const replace = str => {
+  var newstring = "";
+
+  for(var i = 0; i < str.length; i++){
+      if(str[i] == " ")
+          newstring +=  "%20";
+      else
+          newstring +=  str[i];
+      }
+  return newstring;
+}
+
+
+console.log(replace("a d f"));
+
