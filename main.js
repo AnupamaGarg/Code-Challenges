995a231ca6f0ffc5e8b61cd59e3dc7795cccead9
anupamagarg <dolly.singla@gmail.com>
2020-07-02 23:51:58 -0500

    trying the solution

--- main.js
+++ main.js
@@ -632,3 +632,18 @@ console.log(count_duplicate_characters('adsjfdsfsfjsdjfhacabcsbajda'));
 
 
 //Challenge # 26— Sorting an array of strings by length
+//Input : ["You", "are", "beautiful", "looking"]
+//Output : [“You", "are", "looking", "beautiful"]
+
+const sort = function(arr) {
+
+  for (let i = 0; i < arr.length; i ++ ){
+      var temp = arr[i]
+      var j = i - 1
+      while (j >= 0 && temp.length < arr[j].length) {
+          arr[j+1] = arr[j]
+          j--
+      }
+      arr[j+1] = temp
+  }
+}
